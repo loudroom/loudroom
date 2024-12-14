c4038471dc7b69de800b39fbf77b1e8a645024f6
loudroom <loudroomcreations@hotmail.com>
2024-12-14 20:38:03 +1030

    Update ScriptGetPuDuToBalance.js

--- ScriptGetPuDuToBalance.js
+++ ScriptGetPuDuToBalance.js
@@ -56,7 +56,8 @@ async function getPuDuToPreBalanceAndStats() {
             document.getElementById('balance-display').querySelector('span').innerText = formattedBalance;
             document.getElementById('total-supply').querySelector('span').innerText = formattedTotalSupply;
             document.getElementById('percentage-held').querySelector('span').innerText = `${percentageHeld}%`;
-            document.getElementById('return-amount').querySelector('span').innerText = `${percentageHeld}%`;
+            const formattedBalanceInDollars = `$${(formattedBalance * 0.01).toFixed(2)}`;
+            document.getElementById('return-amount').querySelector('span').innerText = formattedBalanceInDollars;
         } catch (error) {
             console.error("Error fetching data from the contract:", error);
             alert("Failed to fetch balance or total supply. Please try again.");
